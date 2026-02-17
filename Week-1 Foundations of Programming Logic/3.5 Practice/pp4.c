d1c566fc603844cad067d6c19eb48b5ea6b5de74
sniekdhoshafiq <developershafiq9578@gmail.com>
2026-02-18 00:18:35 +0600

    Added practice of loops

---
+++ Week-1 Foundations of Programming Logic/3.5 Practice/pp4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    long long int b;
+    float c;
+    char d;
+
+    scanf("%d %lld %f %c", &a, &b, &c, &d);
+
+    printf("%d\n", a);
+    printf("%lld\n", b);
+    printf("%.2f\n", c);
+    printf("%c\n", d);
+
+    return 0;
+}
