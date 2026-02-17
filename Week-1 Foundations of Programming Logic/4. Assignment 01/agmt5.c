3988db0dafe684e87f6d4e4112425b45efddefde
sniekdhoshafiq <developershafiq9578@gmail.com>
2026-02-18 00:40:16 +0600

    Added assignment 01 of week 01

---
+++ Week-1 Foundations of Programming Logic/4. Assignment 01/agmt5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    long long int initialBalance;
+    scanf("%lld", &initialBalance);
+
+    if(initialBalance > 1000)
+    {
+        printf("I will buy Punjabi \n");
+
+        long long int newBalance = initialBalance - 1000;
+
+        if(newBalance >= 500)
+        {
+            printf("I will buy new shoes \n");
+            printf("Alisa will buy new shoes \n");
+        }
+    }
+    else
+    {
+        printf("Bad luck!");
+    }
+
+    return 0;
+}
