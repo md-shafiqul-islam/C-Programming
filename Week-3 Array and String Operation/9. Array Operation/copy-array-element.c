d66c8666732f4ddb3f418f3436cba9b412858f7c
sniekdhoshafiq <developershafiq9578@gmail.com>
2026-02-20 01:41:31 +0600

    Added array operation codes

---
+++ Week-3 Array and String Operation/9. Array Operation/copy-array-element.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    int m;
+    scanf("%d", &m);
+
+    int b[m];
+    for(int i=0; i<m; i++){
+        scanf("%d", &b[i]);
+    }
+
+    int ans[n+m];
+    for(int i=0; i<n; i++){
+        ans[i] = a[i];
+    }
+
+    int x = n;
+    for(int j=0; j<m; j++){
+        ans[x] = b[j];
+        x++;
+    }
+
+    for(int i=0; i<n+m; i++){
+        printf("%d ", ans[i]);
+    }
+
+    return 0;
+}
